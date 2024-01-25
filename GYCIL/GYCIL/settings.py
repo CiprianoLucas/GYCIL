9bd666e0c8b53d4c463ae84ce6a0ac3f4ce07c1b
Lucas Cipriano <lucashcipriano14@gmail.com>
2024-01-25 19:44:48 -0300

    feat: aberto view companies

--- GYCIL/GYCIL/settings.py
+++ GYCIL/GYCIL/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'companies.apps.CompaniesConfig'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'GYCIL.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'base' / 'templates'
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'base' / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- GYCIL/companies/views.py
+++ GYCIL/companies/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+
+from .models import Company
 
 # Create your views here.
+def index(request):
+    companies = Company.objects.order_by("-id")
+        
+    # Aplicando a paginação
+    paginator = Paginator(companies, 2)
+    # /fornecedores?page=1 -> Obtendo a página da URL
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    
+    context = { "companies": page_obj }
+    
+    return render(request, "companies/index.html", context)
