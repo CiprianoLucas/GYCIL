4e0182bde981136009819c66b9cd84ce1b8b087c
Yuri Campos <Yuri_Silva_Campos@outlook.com>
2024-02-19 21:25:58 -0300

    feat: modificações

--- GYCIL/login/urls.py
+++ GYCIL/login/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from . import views
 
-
 app_name = "login"
 urlpatterns = [
-    path('users/', views.UserLoginView.as_view(), name='login'),
-    path('company/', views.login_company, name='login')   
+    path('users/', views.UserLoginView.as_view(), name='users'),
+    path('company/', views.CompanyLoginView.as_view(), name='company')
 ]
