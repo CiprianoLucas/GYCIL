ed469dec55f69f62f6bacc5d804e7c35629217ae
Yuri Campos <Yuri_Silva_Campos@outlook.com>
2024-02-21 23:44:26 -0300

    feat: login_unico

--- GYCIL/login/models.py
+++ GYCIL/login/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.TextField()
+    senha = models.CharField(max_length=100)
--- GYCIL/login/views.py
+++ GYCIL/login/views.py
@@ -1,53 +1,23 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
-from django.views import View
-from django.http import HttpResponse
+from .forms import LoginForm
 
-class UserLoginView(View):
-    template_name = "auth/loginUser.html"
-    next_page = 'companies:index'
-    redirect_authenticated_user = True
+def login_view(request):
+    error_message = None  # Definindo a variável error_message com None inicialmente
 
-    def get(self, request):
-        error_message = None
-        return self.mostrar_pagina_login_user(request, error_message)
-
-    def post(self, request):
-        email_user = request.POST.get('email_user')
-        password_user = request.POST.get('password_user')
-        user = authenticate(request, email_user=email_user, password_user=password_user)
-        
-        if user is not None:
-            login(request, user)
-            return redirect('companies:index')
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            senha = form.cleaned_data['senha']
+            user = authenticate(request, email=email, senha=senha)
+            if user is not None:
+                login(request, user)
+                return redirect('companies:create')  
+            else:
+                error_message = 'Credenciais inválidas. Por favor, tente novamente.'
         else:
-            error_message = "Email ou senha incorretos. Por favor, tente novamente."
-            return self.mostrar_pagina_login_user(request, error_message)
-
-    def mostrar_pagina_login_user(self, request, error_message=None):
-        return render(request, self.template_name, {'error_message': error_message})
-
-
-class CompanyLoginView(View):
-    template_name = "auth/loginCompany.html"
-    next_page = 'companies:index'
-
-    def get(self, request):
-        error_message = None
-        return self.mostrar_pagina_login_company(request, error_message)
-
-    def post(self, request):
-        email_company = request.POST.get('email_company')
-        cnpj = request.POST.get('cnpj')
-        password_company = request.POST.get('password_company')
-        company = authenticate(request, email_company=email_company, cnpj=cnpj, password_company=password_company)
-
-        if company is not None:
-            login(request, company)
-            return HttpResponse('Login realizado com sucesso!')
-        else:
-            error_message = "Email ou senha incorretos. Por favor, tente novamente."
-            return self.mostrar_pagina_login_company(request, error_message)
-
-    def mostrar_pagina_login_company(self, request, error_message=None):
-        return render(request, self.template_name, {'error_message': error_message})
+            error_message = 'Por favor, corrija os erros no formulário.'
+    else:
+        form = LoginForm()
+    return render(request, 'auth/login.html', {'form': form, 'error_message': error_message})
